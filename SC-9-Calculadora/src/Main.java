9e3854b9d97005374abf167edd8784e8013d9663
André Torres <150715327+andtors@users.noreply.github.com>
2025-03-15 14:40:39 -0300

    SC-9-Concluido

--- SC-9-Calculadora/src/Main.java
+++ SC-9-Calculadora/src/Main.java
@@ -1,5 +1,41 @@
+import java.util.Scanner;
+
 public class Main {
+
+    public static String calculo(String operacao, double num1, double num2){
+        double resultado = 0;
+        if(operacao.equals("+")){
+            resultado = num1 + num2;
+            return "O resultado da operação " + num1 + " " + operacao + " " + num2 + " = " + resultado;
+        } else if (operacao.equals("-")) {
+            resultado = num1 - num2;
+            return "O resultado da operação " + num1 + " " + operacao + " " + num2 + " = " + resultado;
+        } else if(operacao.equals("*")){
+            resultado = num1 * num2;
+            return "O resultado da operação " + num1 + " " + operacao + " " + num2 + " = " + resultado;
+        } else if(operacao.equals("/")){
+            resultado = num1 / num2;
+            return "O resultado da operação " + num1 + " " + operacao + " " + num2 + " = " + resultado;
+        } else {
+            return "Operação invalida";
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.printf("Qual operação deseja realizar? Soma(+), Subtração(-), Multiplicação(*) ou Divisão(/): ");
+        String operacao = scanner.next();
+
+        System.out.printf("Insira o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.printf("Insira o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.printf(calculo(operacao, num1, num2));
+
+
     }
 }
